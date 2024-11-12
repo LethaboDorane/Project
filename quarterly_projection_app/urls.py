005a2183b74e9f1850f3834e951216c87708c16c
LethaboDorane <lethabodorane12345@gmail.com>
2024-11-12 15:36:46 +0200

    Update urls.py

--- quarterly_projection_app/urls.py
+++ quarterly_projection_app/urls.py
@@ -1,9 +1,8 @@
 # quarterly_projection_app/urls.py
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('projection/', views.quarterly_projection_view, name='quarterly_projection'),
+    path('projection/', views.quarterly_projection_view, name='quarterly_projection'),  # Ensure this name matches
     path('download_graph/', views.download_graph, name='download_graph'),
 ]
