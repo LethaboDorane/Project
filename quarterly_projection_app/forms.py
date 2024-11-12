c650b26180f5b6f247a64844be9187fd533e93c3
LethaboDorane <lethabodorane12345@gmail.com>
2024-11-12 15:14:29 +0200

    Quarterly projection app

---
+++ quarterly_projection_app/forms.py
@@ -0,0 +1,22 @@
+# quarterly_projection_app/forms.py
+from django import forms
+
+class QuarterlyDataForm(forms.Form):
+    sales_data = forms.CharField(
+        widget=forms.Textarea(attrs={'placeholder': 'Enter sales data in CSV format (Quarter, Year, Sales)'}),
+        # help_text="Enter sales data in CSV format (Quarter, Year, Sales)."
+    )
+    expenses_data = forms.CharField(
+        widget=forms.Textarea(attrs={'placeholder': 'Optional: Enter expenses data in CSV format.'}),
+        required=False,
+        # help_text="Optional: Enter expenses data in CSV format."
+    )
+    quarters = forms.IntegerField(
+        widget=forms.NumberInput(attrs={'placeholder': 'Number of quarters to project'}),
+        # help_text="Number of quarters to project."
+    )
+    graph_type = forms.ChoiceField(
+        choices=[('bar', 'Bar'), ('line', 'Line'), ('scatter', 'Scatter')],
+        widget=forms.Select(attrs={'placeholder': 'Select the graph type'}),
+        # help_text="Select the graph type."
+    )
