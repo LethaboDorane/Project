e7618075dce24708fa65b7f535400a8832bc3622
LethaboDorane <lethabodorane12345@gmail.com>
2024-11-12 19:00:45 +0200

    Delete quarterly_projection_app directory

--- quarterly_projection_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class QuarterlyProjectionAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'quarterly_projection_app'
--- quarterly_projection_app/urls.py
+++
@@ -1,8 +0,0 @@
-# quarterly_projection_app/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('projection/', views.quarterly_projection_view, name='quarterly_projection'),  # Ensure this name matches
-    path('download_graph/', views.download_graph, name='download_graph'),
-]
